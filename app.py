596fe3af06653ff0b48c9241e1e6b160dd3541ce
Meyer <ehlmeyer@gmail.com>
2019-06-13 09:20:35 -0500

    not sure

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost",
+                        database="pet_hotel"
+
+
+                        )
+
+cur = conn.cursor()
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return 'Server Works!'
-  
+@app.route('/test')
+def test_get():
+  cur.execute(
+      "SELECT * FROM pet;")
+  rows = cur.fetchall()
+  print(rows)
+  # tupleRows = tuple(rows)
+  return(rows)
+
 @app.route('/greet')
 def say_hello():
   return 'Hello from Server'
+
+
+conn.commit()
