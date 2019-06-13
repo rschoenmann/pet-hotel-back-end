560b1f1288578bf7f6a44c66438202ecdfb4dea2
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-06-13 11:28:49 -0500

    completed get request with object keys

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import psycopg2
+import psycopg2.extras
+
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -6,7 +8,7 @@ conn = psycopg2.connect(host="localhost",
                         database="pet_hotel"
                         )
 
-cur = conn.cursor()
+cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 
 cur.execute('SELECT * FROM pet')
@@ -16,17 +18,19 @@ cur.fetchone()
 def getPet():
 	if request.method == 'GET':
 		cur.execute('SELECT * FROM pet')
+
 		rows = jsonify(cur.fetchall())
-		print(rows[0])
-		return jsonify(rows)
-		
+		print(rows)
+		return (rows)
+
 
 cur.execute('SELECT * FROM pet')
 rows = cur.fetchall()
 print(rows)
 
 conn.commit()
-  
+
+
 @app.route('/greet')
 def say_hello():
   return 'Hello from Server'
