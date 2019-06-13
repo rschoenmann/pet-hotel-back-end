27105518289d525c05dadf01116f4ade915d793a
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-06-13 11:27:28 -0500

    completed get request

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import psycopg2
+import psycopg2.extras
+
+# import pprint
+
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -6,27 +10,33 @@ conn = psycopg2.connect(host="localhost",
                         database="pet_hotel"
                         )
 
-cur = conn.cursor()
+cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 
 cur.execute('SELECT * FROM pet')
 cur.fetchone()
+# pprint.pprint(cur.description[0].type_code)
+# column_names = [desc[0] for desc in cur.description]
 
 @app.route('/', methods=['GET', 'POST'])
 def getPet():
 	if request.method == 'GET':
 		cur.execute('SELECT * FROM pet')
+    
 		rows = jsonify(cur.fetchall())
-		print(rows[0])
-		return jsonify(rows)
-		
+		print(rows)
+		return (rows)
+
+
+
 
 cur.execute('SELECT * FROM pet')
 rows = cur.fetchall()
 print(rows)
 
 conn.commit()
-  
+
+
 @app.route('/greet')
 def say_hello():
   return 'Hello from Server'
